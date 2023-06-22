2382bb8b8c091525c5dd4c83fc9f2d3ddec65c05
jinseokSong <me123alsl@gmail.com>
2023-06-22 11:13:12 +0900

    리드미 문서 수정

--- src/main/java/sam/song/product/adapter/out/ProductCustomRepository.java
+++ src/main/java/sam/song/product/adapter/out/ProductCustomRepository.java
@@ -7,9 +7,27 @@ import sam.song.product.adapter.in.request.UpdateProductRequest;
 
 public interface ProductCustomRepository {
 
+  /**
+   * 상품 조건 조회 Repository
+   * @param option
+   * @param pageable
+   * @return 상품엔티티 리스트
+   */
   List<ProductJpaEntity> findAllSearchOption(SearchProductOptionRequest option, Pageable pageable);
 
+  /**
+   * 상품 수정 Repository
+   * @param id
+   * @param request
+   * @return 수정된 상품 엔티티
+   */
   ProductJpaEntity update(long id, UpdateProductRequest request);
 
+  /**
+   * 상품 수량 수정 Repository
+   * @param id
+   * @param quantity
+   * @return 수정된 상품 엔티티
+   */
   ProductJpaEntity updateQuantity(long id, Integer quantity);
 }
--- src/main/java/sam/song/product/adapter/out/ProductJpaRepository.java
+++ src/main/java/sam/song/product/adapter/out/ProductJpaRepository.java
@@ -9,16 +9,47 @@ import sam.song.product.adapter.in.request.SearchProductOptionRequest;
 public interface ProductJpaRepository
     extends JpaRepository<ProductJpaEntity, Long>, ProductCustomRepository {
 
+  /**
+   * 상품 전체 조회 Repository
+   * @return 상품 엔티티 리스트
+   */
   List<ProductJpaEntity> findAll();
 
+  /**
+   * 상품 단건 조회 Repository
+   * @param id must not be {@literal null}.
+   * @return 상품 엔티티 (옵셔널)
+   */
   Optional<ProductJpaEntity> findById(Long id);
+
+  /**
+   * 상품 존재 여부 Repository
+   * @param id must not be {@literal null}.
+   * @return 상품 존재 여부
+   */
   boolean existsById(Long id);
 
+  /**
+   * 상품 저장 Repository
+   * @param productJpaEntity must not be {@literal null}.
+   * @return 저장된 상품 엔티티
+   */
   ProductJpaEntity save(ProductJpaEntity productJpaEntity);
 
+  /**
+   * 상품 삭제 Repository
+   * @param id must not be {@literal null}.
+   */
   @Override
   void deleteById(Long id);
 
+  /**
+   * 상품 조건 조회 Repository
+   * @param option
+   * @param pageable
+   * @return 상품엔티티 리스트
+   */
+  @Override
   List<ProductJpaEntity> findAllSearchOption(SearchProductOptionRequest option, Pageable pageable);
 
 }
