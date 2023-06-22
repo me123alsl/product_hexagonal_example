58d5f2affe294bd0218f5998b6ab7793d15da9f0
jinseokSong <me123alsl@gmail.com>
2023-06-22 15:21:18 +0900

    완료

--- src/main/java/sam/song/product/application/service/ProductService.java
+++ src/main/java/sam/song/product/application/service/ProductService.java
@@ -2,7 +2,6 @@ package sam.song.product.application.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.Synchronized;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
